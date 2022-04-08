8d2b4154a19956617478a7a71978a997ef4bc283
Shanitkk <Shanitk2020@gmail.com>
2022-04-08 22:32:42 +0300

    .

---
+++ src/Game/Movement.java
@@ -0,0 +1,19 @@
+package Game;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Movement implements KeyListener {
+
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
