4a08c156cb4f051329165be3471fc936f4adabce
Shanitkk <Shanitk2020@gmail.com>
2022-04-09 13:43:31 +0300

    .

--- src/Game/GameScene.java
+++ src/Game/GameScene.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class GameScene extends JPanel {
-    public static final int FALLING_ORANGES_AMOUNT = 3;
+    public static final int FALLING_ORANGES_AMOUNT = 2;
 
 
     private Player tapozitPlayer;
@@ -14,29 +14,30 @@ public class GameScene extends JPanel {
 
     public GameScene(int x, int y, int weight, int height) {
         this.setBounds(x, y, weight, height);
+        this.setBackground(new Color(204, 255, 204));
+
         this.tapozitPlayer = new Player();
         this.mainGameScene();
-        Movement movement = new Movement(this.tapozitPlayer);
-        this.addKeyListener(movement);
 
         this.orangesList = new ArrayList<>();
         Random random = new Random();
-        for (int i = 0; i < FALLING_ORANGES_AMOUNT; i++) {
-            Orange orange = new Orange(random.nextInt(Main.WINDOW_WEIGHT),0);
+        for (int i = 0; i <= random.nextInt(FALLING_ORANGES_AMOUNT) + 1; i++) {
+            Orange orange = new Orange(random.nextInt(Main.WINDOW_WEIGHT), 0);
             this.orangesList.add(orange);
         }
 
-        this.setLayout(null);
-        this.setDoubleBuffered(true);
-        this.setBackground(new Color(204, 255, 204));
-        this.setFocusable(true);
-        this.requestFocus();
 
-
-    }
+     }
 
     private void mainGameScene() {
         Thread move = new Thread(() -> {
+            Movement movement = new Movement(this.tapozitPlayer);
+            this.addKeyListener(movement);
+            this.setFocusable(true);
+            this.requestFocus();
+            this.setLayout(null);
+            this.setDoubleBuffered(true);
+
             while (true) {
                 switch (this.tapozitPlayer.getDirection()) {
                     case Player.RIGHT:
@@ -50,7 +51,6 @@ public class GameScene extends JPanel {
                 // צריך לחשב גבול, שהשחקנית לא תצא מהמסך TODO
 
 
-
                 repaint();
                 try {
                     Thread.sleep(Player.PLAYER_SPEED);
@@ -61,25 +61,23 @@ public class GameScene extends JPanel {
             }
         });
         move.start();
+
+        Thread fallingOranges = new Thread(() -> {
+            while (true) {
+                try {
+                    repaint();
+                    for (Orange orange : this.orangesList) {
+                        orange.moveDown();
+                        Thread.sleep(100);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        fallingOranges.start();
     }
-
-//    Thread fallingOranges = new Thread(() -> {
-//        while (true) {
-//            switch () {
-//
-//            repaint();
-//            try {
-//                Thread.sleep(300);
-//
-//            } catch (InterruptedException e) {
-//
-//                e.printStackTrace();
-//            }
-//        }
-//    });
-//        move.start();
-//}
-
 
     public void paintComponent(Graphics graphics) {
         super.paintComponent(graphics);
@@ -87,6 +85,6 @@ public class GameScene extends JPanel {
         for (Orange orange : orangesList) {
             orange.paint(graphics);
         }
-      }
+    }
 }
 
--- src/Game/Orange.java
+++ src/Game/Orange.java
@@ -8,19 +8,11 @@ public class Orange {
     private Circle leaf;
     private int direction;
 
-    public static final int ORANGE_X = 20;
-    public static final int ORANGE_Y = 30;
     public static final int ORANGE_WEIGHT = 45;
     public static final int ORANGE_HEIGHT = 45;
     public static final int LEAF_MARGIN = 3;
     public static final int LEAF_WEIGHT = 6;
     public static final int LEAF_HEIGHT = 12;
-
-    public Orange() {
-        this.orange = new Circle(ORANGE_X, ORANGE_Y, ORANGE_WEIGHT, ORANGE_HEIGHT, new Color(255, 154, 0));
-        this.leaf = new Circle(this.orange.getX() + (this.orange.getWidth() / 2),
-                this.orange.getY() + LEAF_MARGIN, LEAF_WEIGHT, LEAF_HEIGHT, new Color(0, 204, 0));
-    }
 
     public Orange(int x, int y) {
         this.orange = new Circle(x, y, ORANGE_WEIGHT, ORANGE_HEIGHT, new Color(255, 154, 0));
@@ -28,11 +20,17 @@ public class Orange {
                 this.orange.getY() + LEAF_MARGIN, LEAF_WEIGHT, LEAF_HEIGHT, new Color(0, 204, 0));
     }
 
+    public void moveDown()
+    {
+        this.orange.setY(this.orange.getY()+1);
+        this.leaf.setY(this.leaf.getY()+1);
+    }
 
     public void paint(Graphics graphics) {
         this.orange.paint(graphics);
         this.leaf.paint(graphics);
     }
+
 
     public int getDirection() {
         return direction;
